3aa4174d9f9c83d544d584d1c7428338688af8ec
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-01-22 11:00:37 +0000

    Boilerplate to set-up Puppet VMs
    Sets up:
     - VM
     - Puppet inside aforementioned VM
     - Puppet module inside Puppet installation on aforementioned VM

---
+++ skeleton/spec/spec_helper_acceptance.rb
@@ -0,0 +1,26 @@
+require 'beaker-rspec'
+require 'pry'
+
+hosts.each do |host|
+  install_package host, 'rubygems'
+  on host, 'gem install puppet --no-ri --no-rdoc'
+  on host, "mkdir -p #{host['distmoduledir']}"
+end
+
+RSpec.configure do |c|
+
+  # Set project root directory
+  proj_root = File.expand_path(File.open(File.dirname(__FILE__), '..'))
+
+  # Enforce readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all the nodes
+  c.before :suite do
+    # Install relevant Puppet module(s)
+    puppet_mdoule_install(:source => proj_root, :module_name => 'skeleton')
+    hosts.each do |host|
+      on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+    end
+  end
+end
