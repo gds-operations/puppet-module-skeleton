e5bf39c5e14039f87fbe335a1fc8b42689237fb1
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-01-29 11:56:23 +0000

    Remove Rspec::Puppet::Coverage temporarily
    rspec-puppet is pinned at 1.0.1, which is the latest currently available
    from RubyGems, and the coverage report requires a newer version.

--- skeleton/spec/classes/coverage_spec.rb
+++ skeleton/spec/classes/coverage_spec.rb
@@ -1,1 +1,4 @@
-at_exit { RSpec::Puppet::Coverage.report! }
+# RSpec::Puppet::Coverage requires a version of rspec-puppet later than 1.0.1,
+# which has not been released yet.
+
+# at_exit { RSpec::Puppet::Coverage.report! }
